854e0e4327bc86e9881828baa79dd2f66f055f72
echoi <echoi@echoi-VirtualBox.(none)>
2014-12-18 02:47:08 -0600

    create builder package

---
+++ src/mysd/builder/Builder.java
@@ -0,0 +1,11 @@
+
+public class Builder{
+
+    
+    public void buildLattice(List<LatticeSite> basis, 
+                             Unitcell unitCell, int nx, int ny, int nz){
+
+     
+
+    }
+}
