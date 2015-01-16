79626426355ea1c1ab530f482c7e438e5c46245b
Eunsong <eunsong.choi@gmail.com>
2015-01-16 16:05:25 -0600

    breakdown lines into multi-lines in SigmaHamiltonian.java

--- src/mysd/SigmaHamiltonian.java
+++ src/mysd/SigmaHamiltonian.java
@@ -67,9 +67,23 @@ public class SigmaHamiltonian extends Interaction<SigmaSpinSite>{
         double zbj = zj.getY();
         double zcj = zj.getZ();
 
-        double Fx = (((matrix[1][0]*zaj + matrix[1][1]*zbj + matrix[1][2]*zcj)*xai - (matrix[0][0]*zaj + matrix[0][1]*zbj + matrix[0][2]*zcj)*xbi)*(-(ybi*zai) + yai*zbi) + ((-(matrix[2][0]*zaj) - matrix[2][1]*zbj - matrix[2][2]*zcj)*xai + (matrix[0][0]*zaj + matrix[0][1]*zbj + matrix[0][2]*zcj)*xci)*(yci*zai - yai*zci) + ((matrix[2][0]*zaj + matrix[2][1]*zbj + matrix[2][2]*zcj)*xbi - (matrix[1][0]*zaj + matrix[1][1]*zbj + matrix[1][2]*zcj)*xci)*(-(yci*zbi) + ybi*zci))/(xci*(-(ybi*zai) + yai*zbi) + xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
+        double Fx = (((matrix[1][0]*zaj + matrix[1][1]*zbj +
+                    matrix[1][2]*zcj)*xai - (matrix[0][0]*zaj + matrix[0][1]*zbj +
+                    matrix[0][2]*zcj)*xbi)*(-(ybi*zai) + yai*zbi) + ((-(matrix[2][0]*zaj) -
+                    matrix[2][1]*zbj - matrix[2][2]*zcj)*xai + (matrix[0][0]*zaj + matrix[0][1]*zbj
+                    + matrix[0][2]*zcj)*xci)*(yci*zai - yai*zci) + ((matrix[2][0]*zaj +
+                    matrix[2][1]*zbj + matrix[2][2]*zcj)*xbi - (matrix[1][0]*zaj + matrix[1][1]*zbj
+                    + matrix[1][2]*zcj)*xci)*(-(yci*zbi) + ybi*zci))/(xci*(-(ybi*zai) + yai*zbi) +
+                    xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
         
-        double Fy = (((matrix[1][0]*zaj + matrix[1][1]*zbj + matrix[1][2]*zcj)*xai - (matrix[0][0]*zaj + matrix[0][1]*zbj + matrix[0][2]*zcj)*xbi)*(xbi*zai - xai*zbi) + ((-(matrix[2][0]*zaj) - matrix[2][1]*zbj - matrix[2][2]*zcj)*xai + (matrix[0][0]*zaj + matrix[0][1]*zbj + matrix[0][2]*zcj)*xci)*(-(xci*zai) + xai*zci) + ((matrix[2][0]*zaj + matrix[2][1]*zbj + matrix[2][2]*zcj)*xbi - (matrix[1][0]*zaj + matrix[1][1]*zbj + matrix[1][2]*zcj)*xci)*(xci*zbi - xbi*zci))/(xci*(-(ybi*zai) + yai*zbi) + xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
+        double Fy = (((matrix[1][0]*zaj + matrix[1][1]*zbj +
+                    matrix[1][2]*zcj)*xai - (matrix[0][0]*zaj + matrix[0][1]*zbj +
+                    matrix[0][2]*zcj)*xbi)*(xbi*zai - xai*zbi) + ((-(matrix[2][0]*zaj) -
+                    matrix[2][1]*zbj - matrix[2][2]*zcj)*xai + (matrix[0][0]*zaj + matrix[0][1]*zbj
+                    + matrix[0][2]*zcj)*xci)*(-(xci*zai) + xai*zci) + ((matrix[2][0]*zaj +
+                    matrix[2][1]*zbj + matrix[2][2]*zcj)*xbi - (matrix[1][0]*zaj + matrix[1][1]*zbj
+                    + matrix[1][2]*zcj)*xci)*(xci*zbi - xbi*zci))/(xci*(-(ybi*zai) + yai*zbi) +
+                    xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
         
         double Fz = 0.0;
         return new Vector3D(Fx, Fy, Fz);
@@ -102,9 +116,23 @@ public class SigmaHamiltonian extends Interaction<SigmaSpinSite>{
         double zbj = zj.getY();
         double zcj = zj.getZ();
 
-        double Fx = ((-(zbi*(matrix[0][0]*xaj + matrix[0][1]*xbj + matrix[0][2]*xcj)) + zai*(matrix[1][0]*xaj + matrix[1][1]*xbj + matrix[1][2]*xcj))*(-(ybi*zai) + yai*zbi) + (zci*(matrix[0][0]*xaj + matrix[0][1]*xbj + matrix[0][2]*xcj) - zai*(matrix[2][0]*xaj + matrix[2][1]*xbj + matrix[2][2]*xcj))*(yci*zai - yai*zci) + (-(zci*(matrix[1][0]*xaj + matrix[1][1]*xbj + matrix[1][2]*xcj)) + zbi*(matrix[2][0]*xaj + matrix[2][1]*xbj + matrix[2][2]*xcj))*(-(yci*zbi) + ybi*zci))/(xci*(-(ybi*zai) + yai*zbi) + xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
+        double Fx = ((-(zbi*(matrix[0][0]*xaj + matrix[0][1]*xbj +
+                    matrix[0][2]*xcj)) + zai*(matrix[1][0]*xaj + matrix[1][1]*xbj +
+                    matrix[1][2]*xcj))*(-(ybi*zai) + yai*zbi) + (zci*(matrix[0][0]*xaj +
+                    matrix[0][1]*xbj + matrix[0][2]*xcj) - zai*(matrix[2][0]*xaj + matrix[2][1]*xbj
+                    + matrix[2][2]*xcj))*(yci*zai - yai*zci) + (-(zci*(matrix[1][0]*xaj +
+                    matrix[1][1]*xbj + matrix[1][2]*xcj)) + zbi*(matrix[2][0]*xaj + matrix[2][1]*xbj
+                    + matrix[2][2]*xcj))*(-(yci*zbi) + ybi*zci))/(xci*(-(ybi*zai) + yai*zbi) +
+                    xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
         
-        double Fy = ((-(zbi*(matrix[0][0]*xaj + matrix[0][1]*xbj + matrix[0][2]*xcj)) + zai*(matrix[1][0]*xaj + matrix[1][1]*xbj + matrix[1][2]*xcj))*(xbi*zai - xai*zbi) + (-(zci*(matrix[0][0]*xaj + matrix[0][1]*xbj + matrix[0][2]*xcj)) + zai*(matrix[2][0]*xaj + matrix[2][1]*xbj + matrix[2][2]*xcj))*(xci*zai - xai*zci) + (-(zci*(matrix[1][0]*xaj + matrix[1][1]*xbj + matrix[1][2]*xcj)) + zbi*(matrix[2][0]*xaj + matrix[2][1]*xbj + matrix[2][2]*xcj))*(xci*zbi - xbi*zci))/(xci*(-(ybi*zai) + yai*zbi) + xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
+        double Fy = ((-(zbi*(matrix[0][0]*xaj + matrix[0][1]*xbj +
+                    matrix[0][2]*xcj)) + zai*(matrix[1][0]*xaj + matrix[1][1]*xbj +
+                    matrix[1][2]*xcj))*(xbi*zai - xai*zbi) + (-(zci*(matrix[0][0]*xaj +
+                    matrix[0][1]*xbj + matrix[0][2]*xcj)) + zai*(matrix[2][0]*xaj + matrix[2][1]*xbj
+                    + matrix[2][2]*xcj))*(xci*zai - xai*zci) + (-(zci*(matrix[1][0]*xaj +
+                    matrix[1][1]*xbj + matrix[1][2]*xcj)) + zbi*(matrix[2][0]*xaj + matrix[2][1]*xbj
+                    + matrix[2][2]*xcj))*(xci*zbi - xbi*zci))/(xci*(-(ybi*zai) + yai*zbi) +
+                    xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
         
         double Fz = 0.0;
         return new Vector3D(Fx, Fy, Fz);
@@ -138,9 +166,23 @@ public class SigmaHamiltonian extends Interaction<SigmaSpinSite>{
         double zbj = zj.getY();
         double zcj = zj.getZ();
 
-        double Fx = (((matrix[1][0]*zaj + matrix[1][1]*zbj + matrix[1][2]*zcj)*yai - (matrix[0][0]*zaj + matrix[0][1]*zbj + matrix[0][2]*zcj)*ybi)*(-(ybi*zai) + yai*zbi) + ((matrix[2][0]*zaj + matrix[2][1]*zbj + matrix[2][2]*zcj)*yai - (matrix[0][0]*zaj + matrix[0][1]*zbj + matrix[0][2]*zcj)*yci)*(-(yci*zai) + yai*zci) + ((matrix[2][0]*zaj + matrix[2][1]*zbj + matrix[2][2]*zcj)*ybi - (matrix[1][0]*zaj + matrix[1][1]*zbj + matrix[1][2]*zcj)*yci)*(-(yci*zbi) + ybi*zci))/(xci*(-(ybi*zai) + yai*zbi) + xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
+        double Fx = (((matrix[1][0]*zaj + matrix[1][1]*zbj +
+                    matrix[1][2]*zcj)*yai - (matrix[0][0]*zaj + matrix[0][1]*zbj +
+                    matrix[0][2]*zcj)*ybi)*(-(ybi*zai) + yai*zbi) + ((matrix[2][0]*zaj +
+                    matrix[2][1]*zbj + matrix[2][2]*zcj)*yai - (matrix[0][0]*zaj + matrix[0][1]*zbj
+                    + matrix[0][2]*zcj)*yci)*(-(yci*zai) + yai*zci) + ((matrix[2][0]*zaj +
+                    matrix[2][1]*zbj + matrix[2][2]*zcj)*ybi - (matrix[1][0]*zaj + matrix[1][1]*zbj
+                    + matrix[1][2]*zcj)*yci)*(-(yci*zbi) + ybi*zci))/(xci*(-(ybi*zai) + yai*zbi) +
+                    xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
         
-        double Fy = (((matrix[1][0]*zaj + matrix[1][1]*zbj + matrix[1][2]*zcj)*yai - (matrix[0][0]*zaj + matrix[0][1]*zbj + matrix[0][2]*zcj)*ybi)*(xbi*zai - xai*zbi) + ((matrix[2][0]*zaj + matrix[2][1]*zbj + matrix[2][2]*zcj)*yai - (matrix[0][0]*zaj + matrix[0][1]*zbj + matrix[0][2]*zcj)*yci)*(xci*zai - xai*zci) + ((matrix[2][0]*zaj + matrix[2][1]*zbj + matrix[2][2]*zcj)*ybi - (matrix[1][0]*zaj + matrix[1][1]*zbj + matrix[1][2]*zcj)*yci)*(xci*zbi - xbi*zci))/(xci*(-(ybi*zai) + yai*zbi) + xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
+        double Fy = (((matrix[1][0]*zaj + matrix[1][1]*zbj +
+                    matrix[1][2]*zcj)*yai - (matrix[0][0]*zaj + matrix[0][1]*zbj +
+                    matrix[0][2]*zcj)*ybi)*(xbi*zai - xai*zbi) + ((matrix[2][0]*zaj +
+                    matrix[2][1]*zbj + matrix[2][2]*zcj)*yai - (matrix[0][0]*zaj + matrix[0][1]*zbj
+                    + matrix[0][2]*zcj)*yci)*(xci*zai - xai*zci) + ((matrix[2][0]*zaj +
+                    matrix[2][1]*zbj + matrix[2][2]*zcj)*ybi - (matrix[1][0]*zaj + matrix[1][1]*zbj
+                    + matrix[1][2]*zcj)*yci)*(xci*zbi - xbi*zci))/(xci*(-(ybi*zai) + yai*zbi) +
+                    xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
         
         double Fz = 0.0;
         return new Vector3D(Fx, Fy, Fz);
@@ -174,12 +216,32 @@ public class SigmaHamiltonian extends Interaction<SigmaSpinSite>{
         double zbj = zj.getY();
         double zcj = zj.getZ();
 
-    double Fx = ((-(zbi*(matrix[0][0]*yaj + matrix[0][1]*ybj + matrix[0][2]*ycj)) + zai*(matrix[1][0]*yaj + matrix[1][1]*ybj + matrix[1][2]*ycj))*(-(ybi*zai) + yai*zbi) + (zci*(matrix[0][0]*yaj + matrix[0][1]*ybj + matrix[0][2]*ycj) - zai*(matrix[2][0]*yaj + matrix[2][1]*ybj + matrix[2][2]*ycj))*(yci*zai - yai*zci) + (-(zci*(matrix[1][0]*yaj + matrix[1][1]*ybj + matrix[1][2]*ycj)) + zbi*(matrix[2][0]*yaj + matrix[2][1]*ybj + matrix[2][2]*ycj))*(-(yci*zbi) + ybi*zci))/(xci*(-(ybi*zai) + yai*zbi) + xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
+        double Fx = ((-(zbi*(matrix[0][0]*yaj + matrix[0][1]*ybj +
+                    matrix[0][2]*ycj)) + zai*(matrix[1][0]*yaj + matrix[1][1]*ybj +
+                    matrix[1][2]*ycj))*(-(ybi*zai) + yai*zbi) + (zci*(matrix[0][0]*yaj +
+                    matrix[0][1]*ybj + matrix[0][2]*ycj) - zai*(matrix[2][0]*yaj + matrix[2][1]*ybj
+                    + matrix[2][2]*ycj))*(yci*zai - yai*zci) + (-(zci*(matrix[1][0]*yaj +
+                    matrix[1][1]*ybj + matrix[1][2]*ycj)) + zbi*(matrix[2][0]*yaj + matrix[2][1]*ybj
+                    + matrix[2][2]*ycj))*(-(yci*zbi) + ybi*zci))/(xci*(-(ybi*zai) + yai*zbi) +
+                    xbi*(yci*zai - yai*zci) + xai*(-(yci*zbi) + ybi*zci));
     
-    double Fy = (zai*((-(xbi*(matrix[1][0]*yaj + matrix[1][1]*ybj + matrix[1][2]*ycj)) - xci*(matrix[2][0]*yaj + matrix[2][1]*ybj + matrix[2][2]*ycj))*zai + xai*(matrix[1][0]*yaj + matrix[1][1]*ybj + matrix[1][2]*ycj)*zbi + xai*(matrix[2][0]*yaj + matrix[2][1]*ybj + matrix[2][2]*ycj)*zci) + zci*(xci*((matrix[0][0]*yaj + matrix[0][1]*ybj + matrix[0][2]*ycj)*zai + (matrix[1][0]*yaj + matrix[1][1]*ybj + matrix[1][2]*ycj)*zbi) - (xai*(matrix[0][0]*yaj + matrix[0][1]*ybj + matrix[0][2]*ycj) + xbi*(matrix[1][0]*yaj + matrix[1][1]*ybj + matrix[1][2]*ycj))*zci) + zbi*((-(xai*(matrix[0][0]*yaj + matrix[0][1]*ybj + matrix[0][2]*ycj)) - xci*(matrix[2][0]*yaj + matrix[2][1]*ybj + matrix[2][2]*ycj))*zbi + xbi*((matrix[0][0]*yaj + matrix[0][1]*ybj + matrix[0][2]*ycj)*zai + (matrix[2][0]*yaj + matrix[2][1]*ybj + matrix[2][2]*ycj)*zci)))/(xci*(ybi*zai - yai*zbi) + xbi*(-(yci*zai) + yai*zci) + xai*(yci*zbi - ybi*zci));
+        double Fy = (zai*((-(xbi*(matrix[1][0]*yaj + matrix[1][1]*ybj +
+                    matrix[1][2]*ycj)) - xci*(matrix[2][0]*yaj + matrix[2][1]*ybj +
+                    matrix[2][2]*ycj))*zai + xai*(matrix[1][0]*yaj + matrix[1][1]*ybj +
+                    matrix[1][2]*ycj)*zbi + xai*(matrix[2][0]*yaj + matrix[2][1]*ybj +
+                    matrix[2][2]*ycj)*zci) + zci*(xci*((matrix[0][0]*yaj + matrix[0][1]*ybj +
+                    matrix[0][2]*ycj)*zai + (matrix[1][0]*yaj + matrix[1][1]*ybj +
+                    matrix[1][2]*ycj)*zbi) - (xai*(matrix[0][0]*yaj + matrix[0][1]*ybj +
+                    matrix[0][2]*ycj) + xbi*(matrix[1][0]*yaj + matrix[1][1]*ybj +
+                    matrix[1][2]*ycj))*zci) + zbi*((-(xai*(matrix[0][0]*yaj + matrix[0][1]*ybj +
+                    matrix[0][2]*ycj)) - xci*(matrix[2][0]*yaj + matrix[2][1]*ybj +
+                    matrix[2][2]*ycj))*zbi + xbi*((matrix[0][0]*yaj + matrix[0][1]*ybj +
+                    matrix[0][2]*ycj)*zai + (matrix[2][0]*yaj + matrix[2][1]*ybj +
+                    matrix[2][2]*ycj)*zci)))/(xci*(ybi*zai - yai*zbi) + xbi*(-(yci*zai) + yai*zci) +
+                    xai*(yci*zbi - ybi*zci));
 
-    double Fz = 0.0;    
-    return new Vector3D(Fx, Fy, Fz);
+        double Fz = 0.0;    
+        return new Vector3D(Fx, Fy, Fz);
 
     }
 
